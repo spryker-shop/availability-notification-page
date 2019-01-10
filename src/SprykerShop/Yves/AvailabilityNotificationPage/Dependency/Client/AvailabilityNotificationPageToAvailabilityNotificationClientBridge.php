5e51b34ae49e2e42be62f144f975794b2fa43634
dpodlesny <denis.podlesny@spryker.com>
2019-01-10 19:54:56 +0200

    CC-2049 Email with unsubscription link

---
+++ src/SprykerShop/Yves/AvailabilityNotificationPage/Dependency/Client/AvailabilityNotificationPageToAvailabilityNotificationClientBridge.php
@@ -0,0 +1,49 @@
+<?php
+
+/**
+ * Copyright © 2016-present Spryker Systems GmbH. All rights reserved.
+ * Use of this software requires acceptance of the Evaluation License Agreement. See LICENSE file.
+ */
+
+namespace SprykerShop\Yves\AvailabilityNotificationPage\Dependency\Client;
+
+use Generated\Shared\Transfer\AvailabilitySubscriptionExistenceRequestTransfer;
+use Generated\Shared\Transfer\AvailabilitySubscriptionExistenceResponseTransfer;
+use Generated\Shared\Transfer\AvailabilitySubscriptionResponseTransfer;
+use Generated\Shared\Transfer\AvailabilitySubscriptionTransfer;
+
+class AvailabilityNotificationPageToAvailabilityNotificationClientBridge implements AvailabilityNotificationPageToAvailabilityNotificationClientInterface
+{
+    /**
+     * @var \Spryker\Client\AvailabilityNotification\AvailabilityNotificationClientInterface
+     */
+    protected $availabilityNotificationClient;
+
+    /**
+     * @param \Spryker\Client\AvailabilityNotification\AvailabilityNotificationClientInterface $availabilityNotificationClient
+     */
+    public function __construct($availabilityNotificationClient)
+    {
+        $this->availabilityNotificationClient = $availabilityNotificationClient;
+    }
+
+    /**
+     * @param \Generated\Shared\Transfer\AvailabilitySubscriptionTransfer $availabilityNotificationSubscriptionRequest
+     *
+     * @return \Generated\Shared\Transfer\AvailabilitySubscriptionResponseTransfer
+     */
+    public function unsubscribe(AvailabilitySubscriptionTransfer $availabilityNotificationSubscriptionRequest): AvailabilitySubscriptionResponseTransfer
+    {
+        return $this->availabilityNotificationClient->unsubscribe($availabilityNotificationSubscriptionRequest);
+    }
+
+    /**
+     * @param \Generated\Shared\Transfer\AvailabilitySubscriptionExistenceRequestTransfer $availabilitySubscriptionExistenceRequestTransfer
+     *
+     * @return \Generated\Shared\Transfer\AvailabilitySubscriptionExistenceResponseTransfer
+     */
+    public function checkExistence(AvailabilitySubscriptionExistenceRequestTransfer $availabilitySubscriptionExistenceRequestTransfer): AvailabilitySubscriptionExistenceResponseTransfer
+    {
+        return $this->availabilityNotificationClient->checkExistence($availabilitySubscriptionExistenceRequestTransfer);
+    }
+}
